875aa02edeef68dd9b533eefb7365d90764f31e6
sundong <sundong@yingu.com>
2017-02-10 09:39:46 +0800

    Initial commit

---
+++ app/src/main/java/com/lewish/start/sharedsdkdemo/MainActivity.java
@@ -0,0 +1,53 @@
+package com.lewish.start.sharedsdkdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn_share;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ShareSDK.initSDK(this, "1b34b1d138b00");
+        btn_share = (Button) findViewById(R.id.btn_share);
+        btn_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showShare();
+            }
+        });
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+         //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
+        oks.setTitle("标题");
+        // titleUrl是标题的网络链接，QQ和QQ空间等使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("【银谷在线】是银谷旗下互联网金融平台，同时获得ICP，CFCA双证！500元起投，期限灵活，为你而来，总有一款适合你。赶快来银谷在线参与吧，新手还有豪礼相送，别让钱躺着睡觉缩水了！");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+        // 启动分享GUI
+        oks.show(this);
+    }
+}
---
+++ app/src/main/java/com/lewish/start/sharedsdkdemo/MyApplication.java
@@ -0,0 +1,14 @@
+package com.lewish.start.sharedsdkdemo;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/2/9 16:45.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
